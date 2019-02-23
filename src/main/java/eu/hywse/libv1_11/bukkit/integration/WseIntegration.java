1ddf4d2ce9fe1e518be9f0b202c4a177b60220ff
hyWse <admin@hywse.eu>
2019-02-23 16:52:24 +0100

    Close statement on log

---
+++ src/main/java/eu/hywse/libv1_11/bukkit/integration/WseIntegration.java
@@ -0,0 +1,48 @@
+package eu.hywse.libv1_11.bukkit.integration;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WseIntegration<I> {
+
+    private boolean enabled;
+
+    private JavaPlugin plugin;
+    private TargetPlugin target;
+
+    private I integration;
+
+    public WseIntegration(JavaPlugin plugin, TargetPlugin target, IntegrationCallback<I> found) {
+        this.plugin = plugin;
+        this.target = target;
+
+        check(found);
+    }
+
+    public void check(IntegrationCallback<I> found) {
+
+        System.out.println("[WseIntegration @ " + plugin.getDescription().getName() + "] Checking integrations...");
+
+        if(!target.check(plugin.getServer())) {
+            System.out.println("[WseIntegration @ " + plugin.getDescription().getName() + "] Integrations: " + String.join(", ", target.getTargets()) + " not found!");
+            return;
+        }
+
+        System.out.println("[WseIntegration @ " + plugin.getDescription().getName() + "] Integrations: " + String.join(", ", target.getTargets()) + " found!");
+
+        enabled = true;
+        this.integration = found.found();
+    }
+
+    public I getIntegration() {
+        return this.integration;
+    }
+
+    public interface IntegrationCallback<I> {
+        I found();
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+}
